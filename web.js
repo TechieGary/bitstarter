67aa251a7836ae378d205cd58f51ccdd2f02c075
TechieGary <gary@kaliware.com>
2013-07-08 00:46:22 +0000

    added fs.read

--- web.js
+++ web.js
@@ -5,8 +5,10 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   var buf = new Buffer(100);
-  buf = fs.readFileSync("./index.html","utf8");
+  fs.readFile('./index.html', function (err, buf) {
+  if (err) throw err;
   response.send(buf.toString('utf8', 0, buf.length));
+}); 
 });
 
 var port = process.env.PORT || 5000;
